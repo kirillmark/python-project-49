e7fc0b9387dc20cc682a45ace473aa7ccc5d3d98
Андрей Марк <markandreym@gmail.com>
2023-01-14 09:04:56 +0300

    Changes7

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-
+import prompt
 
 def main():
     print('Welcome to the Brain Games!')
+    user_name = prompt.string('May I have your name? ')
+    print(f'Hello, {user_name}!')
 
 
 if __name__ == '__main__':
